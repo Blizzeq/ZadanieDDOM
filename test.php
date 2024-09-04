9467b3f00a65207ccc6c5d233436103ce5e8d0eb
Jakub Krasuski <kubamax4@gmail.com>
2024-09-04 13:39:47 +0200

    Add email and password validation test data in test.php

--- test.php
+++ test.php
@@ -4,3 +4,19 @@ require_once 'UserValidator.php';
 
 $validator = new UserValidator();
 
+$emails = [
+    'test@example.com',
+    'invalid.email',
+    'user123@domain.co',
+    '@missingusername.com',
+    'nodomainpart@.com',
+];
+
+$passwords = [
+    'StrongPass1!',
+    'weakpassword',
+    'NoSpecialChar1',
+    'Short1!',
+    'NOLOWERCASE1!',
+    'nouppercase1!',
+];
