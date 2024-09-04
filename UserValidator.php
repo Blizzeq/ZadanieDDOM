b32df5571b4cee9c09c9d8cbd76c4b29d1634141
Jakub Krasuski <kubamax4@gmail.com>
2024-09-04 13:37:26 +0200

    add password validation to UserValidator class

--- UserValidator.php
+++ UserValidator.php
@@ -7,4 +7,19 @@ class UserValidator
         $pattern = '/^[a-zA-Z]+[\w.-]*@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/';
         return (bool) preg_match($pattern, $email);
     }
+
+    public function validatePassword(string $password): bool
+    {
+        $minLength = 8;
+        $hasUppercase = preg_match('/[A-Z]/', $password);
+        $hasLowercase = preg_match('/[a-z]/', $password);
+        $hasDigit = preg_match('/\d/', $password);
+        $hasSpecialChar = preg_match('/[!@#$%^&*(),.?":{}|<>]/', $password);
+
+        return strlen($password) >= $minLength &&
+               $hasUppercase &&
+               $hasLowercase &&
+               $hasDigit &&
+               $hasSpecialChar;
+    }
 }
